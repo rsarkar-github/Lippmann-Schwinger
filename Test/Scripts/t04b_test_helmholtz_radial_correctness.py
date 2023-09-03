1f951319fe9a4347606c109b7d4e8c627e62a564
Rahul Sarkar <rahulsarkar.iitkgp@gmail.com>
2023-09-02 21:23:45 -0700

    Test scripts fixed.

--- Test/Scripts/t04b_test_helmholtz_radial_correctness.py
+++ Test/Scripts/t04b_test_helmholtz_radial_correctness.py
@@ -105,7 +105,7 @@ if __name__ == "__main__":
     # Set grid extent, & calculate minimum grid spacing
     delta_base = lambda_min / 10.0
     a1 = 100 * delta_base  # in km (should not change)
-    a2 = 100 * delta_base  # in km (should not change)
+    a2 = 50 * delta_base  # in km (should not change)
 
     # Grid refining to do (different experiments)
     fac = [1]
