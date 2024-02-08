3ff35467e32d97db2abb82fed74e49eed82fb1c1
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-08 08:57:34 -0800

    Updates p07 final bashscripts

---
+++ Scripts/BashScripts/p07-0-3.sh
@@ -0,0 +1,9 @@
+# Run from within this directory
+
+cd /home/rsarkar/
+source .bashrc
+conda activate py39
+cd /home/rsarkar/Research/Thesis
+python -m Lippmann-Schwinger.Scripts.p07_lse_solves 0 3 0
+python -m Lippmann-Schwinger.Scripts.p07_lse_solves 0 3 1
+python -m Lippmann-Schwinger.Scripts.p07_lse_solves 0 3 2
