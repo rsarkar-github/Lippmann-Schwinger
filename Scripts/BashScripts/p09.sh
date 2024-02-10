983bc337baaa2091fe7f4e1561c0f0bbab8dc97d
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-09 16:51:18 -0800

    Updates

---
+++ Scripts/BashScripts/p09.sh
@@ -0,0 +1,9 @@
+# Run from within this directory
+
+cd /home/rsarkar/
+source .bashrc
+conda activate py39
+cd /home/rsarkar/Research/Thesis
+python -m Lippmann-Schwinger.Scripts.p09_plot_images 0 3
+python -m Lippmann-Schwinger.Scripts.p09_plot_images 1 3
+python -m Lippmann-Schwinger.Scripts.p09_plot_images 2 3
