3058f612b401e88cead920c84e1a2b79c5cbdc28
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-10 05:36:28 -0800

    Updates p11

---
+++ Scripts/BashScripts/p11-2-2.sh
@@ -0,0 +1,9 @@
+# Run from within this directory
+
+cd /home/rsarkar/
+source .bashrc
+conda activate py39
+cd /home/rsarkar/Research/Thesis
+python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 2 2 0
+python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 2 2 1
+python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 2 2 2
