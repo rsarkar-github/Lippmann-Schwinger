70583215fade1dd31464e0ab87d863403889262f
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-02-20 13:01:49 -0800

    Updates + Cleanup

--- Scripts/BashScripts/p11-0-2.sh
+++ Scripts/BashScripts/p11-0-2.sh
@@ -4,6 +4,6 @@ cd /home/rsarkar/
 source .bashrc
 conda activate py39
 cd /home/rsarkar/Research/Thesis
-#python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 0 2 0
-#python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 0 2 1
+python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 0 2 0
+python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 0 2 1
 python -m Lippmann-Schwinger.Scripts.p11_initial_vel_solves 0 2 2
