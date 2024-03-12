43591e5ca81a69c08ab563bd0caf37e564fdd1ed
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-12 16:02:52 -0700

    Fix bashscripts source path issue

--- Scripts/BashScripts/p00.sh
+++ Scripts/BashScripts/p00.sh
@@ -5,6 +5,7 @@ source .bashrc
 conda activate py39
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo $SCRIPT_DIR
 cd $SCRIPT_DIR ..
-cd /home/rsarkar/Research/Thesis
+#cd /home/rsarkar/Research/Thesis
 python -m Lippmann-Schwinger.Scripts.p01_create_vz_marmousi
