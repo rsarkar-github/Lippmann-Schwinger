c1a2f2a332ef07f838ad2f4dc661b3073b58e75f
rsarkar-github <rahulsarkar_iitkgp@hotmail.com>
2024-03-12 17:01:36 -0700

    Bug fix

--- Scripts/BashScripts/p00.sh
+++ Scripts/BashScripts/p00.sh
@@ -6,6 +6,7 @@ conda activate py39
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo $SCRIPT_DIR
-cd $SCRIPT_DIR ..
+cd $SCRIPT_DIR
+pwd
 #cd /home/rsarkar/Research/Thesis
-python -m Lippmann-Schwinger.Scripts.p01_create_vz_marmousi
+#python -m Lippmann-Schwinger.Scripts.p01_create_vz_marmousi
